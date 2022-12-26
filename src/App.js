bd23da1a6e58bce669eb8b4c021830e2cc376c8c
Melny <marek.melnycok@gmail.com>
2022-12-26 23:03:15 +0000

    Link a zmena

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactouš
+          Learn React
         </a>
       </header>
     </div>
